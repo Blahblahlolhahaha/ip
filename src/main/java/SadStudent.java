d3db8f2463d03cf74f13924dff2e5aa9430d3b38
Jess Kwek <pikachuuu1436@gmail.com>
2026-01-14 18:03:31 +0800

    add list

--- src/main/java/SadStudent.java
+++ src/main/java/SadStudent.java
@@ -1,17 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SadStudent {
     public static void main(String[] args) {
-        System.out.println("Hello I'm a sad student! ｡°(°.◜ᯅ◝°)°｡\n How can I help you today?");
+        System.out.println("Hello I'm a sad student! ｡°(°.◜ᯅ◝°)°｡\nHow can I help you today?");
+        ArrayList<Task> tasks = new ArrayList<>();
         while(true){
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            if(input.equals("bye")){
+            if(input.equals("list")) {
+                StringBuffer sb = new StringBuffer();
+                for(Task task : tasks) {
+                    sb.append(task.toString() + "\n");
+                }
+                System.out.println(sb.toString().stripTrailing());
+            }
+            else if(input.equals("bye")) {
                 break;
             }
-            System.out.println(inputsad
-                
-            );
+            else {
+                Task task = new Task(input);
+                tasks.add(task);
+                System.out.println("added: " + input);
+            }
         }
         System.out.println("Alright I go and cry myself to sleep T.T");
     }
