f5ba75a5e2cae5ca82a41c8529ba40661cb8fc6c
Jess Kwek <pikachuuu1436@gmail.com>
2026-01-14 17:41:57 +0800

    Add greetings and rename chatbot to Sad Student

---
+++ src/main/java/SadStudent.java
@@ -0,0 +1,6 @@
+public class SadStudent {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm a sad student! ｡°(°.◜ᯅ◝°)°｡\n How can I help you today?");
+        System.out.println("Alright I go and cry myself to sleep T.T");
+    }
+}
