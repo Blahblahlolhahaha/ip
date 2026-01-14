cf46d66dddbce1f672b25eef28611268f4ed380b
Jess Kwek <pikachuuu1436@gmail.com>
2026-01-14 22:26:54 +0800

    add mark/unmark as done

--- src/main/java/SadStudent.java
+++ src/main/java/SadStudent.java
@@ -3,35 +3,53 @@ import java.util.Scanner;
 public class SadStudent {
     public static void main(String[] args) {
         String text = "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                        "░░░░░░██████░░░░░░░░██████░░░░░░\r\n" + //
-                        "░░░░██░░░░░░░░░░░░░░░░░░░░██░░░░\r\n" + //
-                        "░░██░░░░░░░░░░░░░░░░░░░░░░░░██░░\r\n" + //
-                        "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
-                        "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
-                        "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                        "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                        "░░░░░░░░░░████████████░░░░░░░░░░\r\n" + //
-                        "░░░░░░░░████████████████░░░░░░░░\r\n" + //
-                        "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
-                        "░░░░░░██░░░░░░░░░░░░░░░░██░░░░░░\r\n" + //
-                        "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                        "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                        "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                        "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
+                "░░░░░░██████░░░░░░░░██████░░░░░░\r\n" + //
+                "░░░░██░░░░░░░░░░░░░░░░░░░░██░░░░\r\n" + //
+                "░░██░░░░░░░░░░░░░░░░░░░░░░░░██░░\r\n" + //
+                "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
+                "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░████████████░░░░░░░░░░\r\n" + //
+                "░░░░░░░░████████████████░░░░░░░░\r\n" + //
+                "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
+                "░░░░░░██░░░░░░░░░░░░░░░░██░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
         System.out.println(text + "Hello I'm a sad student\nHow can I help you today?");
         TaskList list = new TaskList();
         Scanner scanner = new Scanner(System.in);
-        while(true){
-            String input = scanner.nextLine();
-            if(input.equals("list")) {
-                
+        while (true) {
+            String input = scanner.nextLine().strip();
+            if (input.equals("list")) {
                 System.out.println(list.toString());
-            }
-            else if(input.equals("bye")) {
+            } else if (input.equals("bye")) {
                 scanner.close();
                 break;
-            }
-            else {
+            } else if (input.startsWith("mark ") || input.startsWith("unmark ")){
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    String ending = "Yay, one task closer to sleeping! :D";
+                    String res = "";
+                    if (input.startsWith("un")) {
+                        ending = "Awwww please dont sike me :(";
+                        res = list.unmark(index);
+                    }
+                    else {
+                        res = list.mark(index);
+                    }
+                    if (res == "") {
+                        System.out.println("Nuuuu index out of range :(");
+                    }
+                    else {
+                        System.out.println(String.format("%s\n%s", ending, res));
+                    }
+                } catch(NumberFormatException e) {
+                    System.out.println("Please mark a valid task!");
+                }
+            } else {
                 list.addTask(input);
                 System.out.println("added: " + input);
             }
