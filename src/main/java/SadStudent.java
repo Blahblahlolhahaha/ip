bb1e4d5fda1c50839202d178c4d0716fc4447897
Jess Kwek <pikachuuu1436@gmail.com>
2026-01-14 17:50:02 +0800

    Add echo

--- src/main/java/SadStudent.java
+++ src/main/java/SadStudent.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class SadStudent {
     public static void main(String[] args) {
         System.out.println("Hello I'm a sad student! ｡°(°.◜ᯅ◝°)°｡\n How can I help you today?");
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if(input.equals("bye")){
+                break;
+            }
+            System.out.println(inputsad
+                
+            );
+        }
         System.out.println("Alright I go and cry myself to sleep T.T");
     }
 }
