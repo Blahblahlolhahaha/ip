16c465e00b53662aed286941ac9dac5bc7a8e85e
Jess Kwek <pikachuuu1436@gmail.com>
2026-01-14 23:36:47 +0800

    Add deadline

--- src/main/java/SadStudent.java
+++ src/main/java/SadStudent.java
@@ -28,7 +28,7 @@ public class SadStudent {
             } else if (input.equals("bye")) {
                 scanner.close();
                 break;
-            } else if (input.startsWith("mark ") || input.startsWith("unmark ")){
+            } else if (input.startsWith("mark ") || input.startsWith("unmark ")) {
                 try {
                     int index = Integer.parseInt(input.split(" ")[1]) - 1;
                     String ending = "Yay, one task closer to sleeping! :D";
@@ -36,22 +36,22 @@ public class SadStudent {
                     if (input.startsWith("un")) {
                         ending = "Awwww please dont sike me :(";
                         res = list.unmark(index);
-                    }
-                    else {
+                    } else {
                         res = list.mark(index);
                     }
                     if (res == "") {
                         System.out.println("Nuuuu index out of range :(");
-                    }
-                    else {
+                    } else {
                         System.out.println(String.format("%s\n%s", ending, res));
                     }
-                } catch(NumberFormatException e) {
+                } catch (NumberFormatException e) {
                     System.out.println("Please mark a valid task!");
                 }
             } else {
-                list.addTask(input);
-                System.out.println("added: " + input);
+                Task task = list.parseTask(input);
+                if (task != null) {
+                    System.out.println("added: " + task.toString());
+                }
             }
         }
         System.out.println("Alright I go and cry myself to sleep T.T");
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -9,11 +9,13 @@ public class TaskList {
 
     public Task parseTask(String input) {
         String taskType = input.split(" ")[0];
-
         switch (taskType) {
             case "todo":
                 return parseTodo(input.substring(input.indexOf("todo ") + 5));
+            case "deadline":
+                return parseDeadline(input.substring(input.indexOf("deadline ") + 9));
             default:
+                System.out.println("I cant support this task type now ;-; Please choose from (todo, deadline)");
                 return null;
         }
     }
@@ -37,9 +39,34 @@ public class TaskList {
     }
 
     private ToDo parseTodo(String input) {
+        if (input.isBlank()) {
+            System.out.println("you specified tasktype but there was no task T.T. You cheated on me!!!");
+            return null;
+        }
         ToDo todo = new ToDo(input);
         tasks.add(todo);
         return todo;
+    }
+
+    private Deadline parseDeadline(String input) {
+        if (input.isBlank()) {
+            System.out.println("you specified tasktype but there was no task T.T. You cheated on me!!!");
+            return null;
+        }
+        String[] params = input.split(" /");
+        String name = params[0];
+        String by = "";
+        for (int i = 1; i < params.length; i++) {
+            if (params[i].startsWith("by ")) {
+                by = params[i].replaceFirst("by ", "");
+            }
+        }
+        if (by.isBlank()) {
+            System.out.println("ehhh Deadline requires by field!\n Usage: deadline <task> /by <by>");
+            return null;
+        }
+        Deadline deadline = new Deadline(name, by);
+        return deadline;
     }
 
     @Override
