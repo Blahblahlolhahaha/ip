b24253230ebf47a707a385d748fe247cacb1c0d6
Jess Kwek <pikachuuu1436@gmail.com>
2026-01-23 17:54:53 +0800

    Extract UI features

--- src/main/java/SadStudent.java
+++ src/main/java/SadStudent.java
@@ -3,50 +3,38 @@ import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class SadStudent {
-    public static void main(String[] args) {
-        String text = "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                "░░░░░░██████░░░░░░░░██████░░░░░░\r\n" + //
-                "░░░░██░░░░░░░░░░░░░░░░░░░░██░░░░\r\n" + //
-                "░░██░░░░░░░░░░░░░░░░░░░░░░░░██░░\r\n" + //
-                "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
-                "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
-                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                "░░░░░░░░░░████████████░░░░░░░░░░\r\n" + //
-                "░░░░░░░░████████████████░░░░░░░░\r\n" + //
-                "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
-                "░░░░░░██░░░░░░░░░░░░░░░░██░░░░░░\r\n" + //
-                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
-                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
-        System.out.println(text + "Hello I'm a sad student\nHow can I help you today?");
+
+    private UI ui;
+
+    public SadStudent() {
+        this.ui = new UI();
+    }
+
+    public void run() {
+        ui.greet();
         Storage storage = new Storage();
         TaskList list = storage.loadFile();
         if (list == null) {
-            System.out.println("An error occured opening the file ;-;");
+            ui.showMessage("An error occured opening the file ;-;");
             return;
         }
-        Scanner scanner = new Scanner(System.in);
         while (true) {
-
             try {
-                String input = scanner.nextLine().strip();
+                String input = ui.getInput();
                 if (input.equals("list")) {
-                    System.out.println(list.toString());
+                    ui.showMessage(list.toString());
                 } else if (input.equals("bye")) {
-                    System.out.println("Alright I go and cry myself to sleep T.T");
-                    scanner.close();
+                    ui.showMessage("Alright I go and cry myself to sleep T.T");
                     break;
                 } else if (input.startsWith("delete")) {
                     int index = Integer.parseInt(input.split(" ")[1]) - 1;
                     Task task = list.removeTask(index);
                     if (task != null) {
-                        System.out.println(
+                        ui.showMessage(
                                 String.format("Task removed: %s\n Out of sight out of mind :D", task.toString()));
 
                     } else {
-                        System.out.println("Nuuuu index out of range :(");
+                        ui.showMessage("Nuuuu index out of range :(");
                     }
                 } else if (input.startsWith("mark ") || input.startsWith("unmark ")) {
                     int index = Integer.parseInt(input.split(" ")[1]) - 1;
@@ -59,9 +47,9 @@ public class SadStudent {
                         res = list.mark(index);
                     }
                     if (res == "") {
-                        System.out.println("Nuuuu index out of range :(");
+                        ui.showMessage("Nuuuu index out of range :(");
                     } else {
-                        System.out.println(String.format("%s\n%s", ending, res));
+                        ui.showMessage(String.format("%s\n%s", ending, res));
                     }
                 } else {
                     Task task = Task.parseTask(input);
@@ -69,20 +57,23 @@ public class SadStudent {
                         list.addTask(task);
                     }
                     if (task != null) {
-                        System.out.println(String.format("added: %s\nYou have %d tasks now!", task.toString(),
+                        ui.showMessage(String.format("added: %s\nYou have %d tasks now!", task.toString(),
                                 list.getNumberOfTasks()));
                     }
                 }
                 storage.storeFile(list);
             } catch (DateTimeParseException e) {
-                System.out.println("Please indicate a valid date!");
+                ui.showMessage("Please indicate a valid date!");
             } catch (NumberFormatException e) {
-                System.out.println("Please indicate a valid task!");
+                ui.showMessage("Please indicate a valid task!");
             } catch (IOException f) {
-                System.out.println("An error occured saving tasks to file ;-;");
+                ui.showMessage("An error occured saving tasks to file ;-;");
 
             }
         }
+    }
 
+    public static void main(String[] args) {
+        new SadStudent().run();
     }
 }
---
+++ src/main/java/UI.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class UI {
+    
+    Scanner scanner;
+
+    public UI() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void showMessage(String output) {
+        System.out.println(output);
+    }
+
+    public void greet() {
+        String text = "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░██████░░░░░░░░██████░░░░░░\r\n" + //
+                "░░░░██░░░░░░░░░░░░░░░░░░░░██░░░░\r\n" + //
+                "░░██░░░░░░░░░░░░░░░░░░░░░░░░██░░\r\n" + //
+                "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
+                "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░████████████░░░░░░░░░░\r\n" + //
+                "░░░░░░░░████████████████░░░░░░░░\r\n" + //
+                "░░░░░░████░░░░░░░░░░░░████░░░░░░\r\n" + //
+                "░░░░░░██░░░░░░░░░░░░░░░░██░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n" + //
+                "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
+        System.out.println(text + "Hello I'm a sad student\nHow can I help you today?");
+    }
+
+    public String getInput() {
+        String input = scanner.nextLine().strip();
+        return input;
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
