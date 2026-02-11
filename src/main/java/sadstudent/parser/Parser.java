b16f742a04bf5ae194552cb4cb28e63225109130
Jess Kwek <pikachuuu1436@gmail.com>
2026-02-11 15:54:10 +0800

    Improve code quality on parseCommand
    Currently parseCommand parses the commands in a
    if else statement together with code inside for
    each command. This affects the readability of the
    code
    
    By extracting out each command as another method
    in Parser, I hope it improves the readability of
    the code. On top of that, I used a switch
    statement instead of an if else block to improve
    readability also

--- src/main/java/sadstudent/parser/Parser.java
+++ src/main/java/sadstudent/parser/Parser.java
@@ -20,49 +20,67 @@ public class Parser {
      * @throws SadStudentException when: index provided for delete/mark/unmark is out of range or command is unsupported
      */
     public static String parseCommand(String input, TaskList list) throws SadStudentException{
-        if (input.equals("list")) {
+        String[] params = input.split(" ");
+        switch(params[0]) {
+        case "list":
             return list.toString();
-        } else if (input.equals("bye")) {
-            // ui.showMessage("Alright I go and cry myself to sleep T.T");
+        case "bye":
             return "";
-        } else if (input.startsWith("delete")) {
-            int index = Integer.parseInt(input.split(" ")[1]) - 1;
-            Task task = list.removeTask(index);
-            if (task != null) {
-                return String.format("Task removed: %s\nOut of sight out of mind :D", task.toString());
-            } else {
-                throw new SadStudentException("Nuuuu index out of range :(");
-            }
-        } else if (input.startsWith("mark ") || input.startsWith("unmark ")) {
-            int index = Integer.parseInt(input.split(" ")[1]) - 1;
-            String ending = "Yay, one task closer to sleeping! :D";
-            String res = "";
-            if (input.startsWith("un")) {
-                ending = "Awwww please dont sike me :(";
-                res = list.unmark(index);
-            } else {
-                res = list.mark(index);
-            }
-            if (res == "") {
-                throw new SadStudentException("Nuuuu index out of range :(");
-            } else {
-                return String.format("%s\n%s", ending, res);
-            }
-        } else if(input.startsWith("find ")) {
-            String search = input.replaceFirst("find ", "");
-            TaskList searchList = list.findTasks(search);
-            if(searchList.getNumberOfTasks() == 0) {
-                return String.format("There are no matches for: %s ;-;", search);
-            }
-            return String.format("tada! Here are the matches for \"%s\":\n%s", search, searchList.toString());
-        } else {
-            Task task = Task.parseTask(input);
-            if (task != null) {
-                list.addTask(task);
-                return String.format("added: %s\nYou have %d tasks now!", task.toString(),
-                        list.getNumberOfTasks());
-            }
-            throw new SadStudentException("An error occured parsing the input ;-;");
+        case "delete":
+            return deleteTask(params, list);
+        case "mark":
+        case "unmark":
+            return markTask(params, list);
+        case "find":
+            return findTask(input, list);
+        default:
+            return addTask(input, list);
         }
+    }
+
+    private static String deleteTask(String[] params, TaskList list) {
+        int index = Integer.parseInt(params[1]) - 1;
+        Task task = list.removeTask(index);
+        if (task != null) {
+            return String.format("Task removed: %s\nOut of sight out of mind :D", task.toString());
+        } else {
+            throw new SadStudentException("Nuuuu index out of range :(");
+        }
+    }
+
+    private static String markTask(String[] params, TaskList list) {
+        int index = Integer.parseInt(params[1]) - 1;
+        String ending = "Yay, one task closer to sleeping! :D";
+        String res = "";
+        if (params[0].startsWith("un")) {
+            ending = "Awwww please dont sike me :(";
+            res = list.unmark(index);
+        } else {
+            res = list.mark(index);
+        }
+        if (res == "") {
+            throw new SadStudentException("Nuuuu index out of range :(");
+        } else {
+            return String.format("%s\n%s", ending, res);
+        }
+    }
+
+    private static String findTask(String input, TaskList list) {
+        String search = input.replaceFirst("find ", "");
+        TaskList searchList = list.findTasks(search);
+        if (searchList.getNumberOfTasks() == 0) {
+            return String.format("There are no matches for: %s ;-;", search);
+        }
+        return String.format("tada! Here are the matches for \"%s\":\n%s", search, searchList.toString());
+    }
+
+    private static String addTask(String input, TaskList list) {
+        Task task = Task.parseTask(input);
+        if (task != null) {
+            list.addTask(task);
+            return String.format("added: %s\nYou have %d tasks now!", task.toString(),
+                    list.getNumberOfTasks());
+        }
+        throw new SadStudentException("An error occured parsing the input ;-;");
     }
 }
